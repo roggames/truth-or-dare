852a5e7d51b5273d562ff13b2ece188b7c272172
mostafahendeli <mostafahendeli@yahoo.com>
2018-10-10 14:48:24 +0330

    rojgames

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1539170148,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bottle-sheet0.png",
+		"images/bottle-sheet1.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/lines-sheet0.png",
+		"images/button-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
